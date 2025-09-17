078ff863b5d659cda75212e8e8a56411b28a3293
Sanjaysai456 <polojisanjaysai9849@gmail.com>
2025-09-17 18:23:03 +0530

    structre is set

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+# Generate log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create 'logs' directory inside current working directory
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Full path of log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] [%(levelname)s] [%(lineno)d] - %(name)s - %(message)s",
+    level=logging.INFO,
+)
+
